ffeaa2b72a0229e326144f4a85ed15ad43b3b65b
Kate <vvellspring@gmail.com>
2022-10-31 19:15:57 +0300

    Задача 57, частотный словарь элементов в массиве

--- Task57/Program.cs
+++ Task57/Program.cs
@@ -34,5 +34,20 @@ void PrintArray(int[,] matrix)
     }
 }
 
-int[,] MyArray = FillArray(row, col);
-PrintArray(MyArray);
+int[,] myArray = FillArray(row, col);
+PrintArray(myArray);
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        if (myArray[i,j] == num)
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine($"Введенное число встречается в массиве {count} раз(а)");
